f0575d19bc4c343bbbe49e771c286bb208f9332e
Vincent G. Guarnaccia <vincent.guarnaccia@gmail.com>
2017-09-11 22:52:14 -0700

    Update

--- publish.sh
+++ publish.sh
@@ -3,13 +3,13 @@
 jupyter-nbconvert --to slides slides.ipynb --reveal-prefix=reveal.js
 mv slides.slides.html  index.html
 mkdir -p /tmp/workspace
-cp -r * /tmp/workspace/
+cp -r ./* /tmp/workspace/
 git add -A .
 git commit -m "Update"
 git checkout -B gh-pages
 cp -r /tmp/workspace/* .
 git add -A .
 git commit -m "new version"
-git push origin master gh-pages
+git push --force origin master gh-pages
 git checkout master
 rm -rf /tmp/workspace
